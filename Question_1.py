f751fc33d4dc4af1e2044f778dd7fe041276a07c
A00467069 <jerry.jebasingh@smu.ca>
2023-12-15 23:10:34 -0400

    Added new stack operation

--- Question_1.py
+++ Question_1.py
@@ -1,9 +1,20 @@
-def stack(new_list, operations, number = None):
+def stack1(new_list, operations, number = None):
     if operations == 'add':
         new_list.insert(0, number)
     elif operations == 'remove':
         if new_list:
             new_list.pop(0)
+        else:
+            print("Stack is empty")
+    return new_list
+#I have created this stack based on the output provided by you in the assignment question
+#I have also created another stack to perform the actual operation
+def stack2(new_list, operations, number = None):
+    if operations == 'add':
+        new_list.append(number)
+    elif operations == 'remove':
+        if new_list:
+            new_list.pop()
         else:
             print("Stack is empty")
     return new_list
@@ -20,12 +31,22 @@ def queue(new_list, operations, number = None):
 
 
 new_list = [1, 2, 3, 4]
+print("Stack Based on desired output given in assignment")
 print("Adding new element to the stack")
-new_list = stack(new_list, 'add', 0)
+new_list = stack1(new_list, 'add', 0)
 print(new_list)
 print("Adding remove an element from stack")
-new_list = stack(new_list, 'remove')
+new_list = stack1(new_list, 'remove')
 print(new_list)
+print("\n")
+print("Stack Based on actual output")
+print("Adding new element to the stack")
+new_list = stack2(new_list, 'add', 0)
+print(new_list)
+print("Adding remove an element from stack")
+new_list = stack2(new_list, 'remove')
+print(new_list)
+print("\n")
 
 print("Adding new element to the queue")
 new_list = queue(new_list, 'add', 5)
